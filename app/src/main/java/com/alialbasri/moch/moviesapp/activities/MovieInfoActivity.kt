f53f4fc23ea0586260cfb3c569217b12a10e90bc
alialbasri88 <thelordtheory@gmai.com>
2018-12-08 13:46:21 +0300

    MovieApp first commit

---
+++ app/src/main/java/com/alialbasri/moch/moviesapp/activities/MovieInfoActivity.kt
@@ -0,0 +1,13 @@
+package com.alialbasri.moch.moviesapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.alialbasri.moch.moviesapp.R
+
+class MovieInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_info)
+    }
+}
---
+++ app/src/main/java/com/alialbasri/moch/moviesapp/activities/SearchActivity.kt
@@ -0,0 +1,13 @@
+package com.alialbasri.moch.moviesapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.alialbasri.moch.moviesapp.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
